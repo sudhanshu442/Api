3dd56530e2a517c2cd4b190157a697f6bbb2a7b4
Lenovo <sudhanshuyadav1665@gmail.com>
2023-05-04 02:15:11 +0530

    own api

---
+++ index.js
@@ -0,0 +1,19 @@
+const express=require('express');
+const cors=require('cors');
+const app=express();
+app.use(cors());
+ const port=process.env.PORT || 3000
+const apiData=require('./data.json');
+ app.get("/",(req,res)=>{
+    res.send("apiData");
+
+ });
+
+ app.get("/service",(req,res)=>{
+    res.send(apiData);
+ })
+
+
+ app.listen(port,()=>{
+    console.log("Server running");
+ })
